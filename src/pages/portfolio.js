26109847ba86207731101fddf6bf7d9a9139c2c4
linda413 <caiqinli99@yahoo.com.tw>
2020-03-20 00:35:42 -0700

    Portfolio Update with Carousel

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,13 +1,49 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Carousel from 'react-bootstrap/Carousel'
 import Layout from "../layouts"
+import BitatUber from "../images/BitatUber.jpg"
+import devTeam from "../images/working.jpg"
+import PostCon from "../images/postcon.jpg"
 
 const Portfolio = () => (
   <Layout>
     <h1> Portfolio</h1>
-    <div> </div>
-    <p>Welcome to my Portfolio</p>
+    <div className="DevBitCarousel">
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src = {BitatUber}
+          alt="WebDevTeamAtUber"
+        />
+        <Carousel.Caption>
+          <h4>OG Web Team</h4>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={devTeam}
+          alt="Working"
+        />
+        <Carousel.Caption>
+          <h4>Working</h4>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={PostCon}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h4>Attending PostCon</h4>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
