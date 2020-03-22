4bce6ec136535123cc330f7b745220b57feaacef
linda413 <caiqinli99@yahoo.com.tw>
2020-03-21 19:22:20 -0700

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,22 @@ import { Link } from "gatsby"
 import Layout from "../layouts"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "animate.css/animate.min.css";
-import {Animated} from "react-animated-css";
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const IndexPage = () => (
   <Layout>
-    <Animated animationIn="fadeInLeft"  animationInDuration={3000} isVisible={true}>
+    <ScrollAnimation animateIn="fadeIn">
       <h1 > Hi there, my name is Linda </h1>
-    </Animated>
+    </ScrollAnimation>
     <br/>
     <br/>
+    <ScrollAnimation animateIn="fadeIn">
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse condimentum mauris nisi, molestie faucibus risus posuere vitae. Proin ac nisl vel erat efficitur aliquet eu ac ante. Pellentesque eu odio in purus volutpat euismod. 
       Aenean ac nunc arcu. Integer eget lorem accumsan, interdum arcu gravida, molestie augue. Morbi sit amet commodo metus, non tincidunt dui. Mauris vitae mi eu lectus ornare aliquam. 
       Sed at vehicula erat, et consectetur neque. Duis tempor, nibh quis sagittis luctus, arcu tellus sodales nunc, non congue quam velit eu massa. 
       Nunc ornare lectus a purus gravida pellentesque. Aliquam viverra sem turpis, vitae aliquet nisl ornare non. Praesent sodales mi nec ultricies posuere. Quisque non ipsum urna. Etiam vestibulum et diam sed posuere. 
       Integer ac velit ut massa blandit dignissim nec ac dui. Fusce tincidunt ullamcorper nunc, sed auctor urna tincidunt eu. </p>
-
+    </ScrollAnimation>
     <Link to="/portfolio">Go to page 2</Link>
   </Layout>
 )
